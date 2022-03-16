75070936e2b6e8cd6ab002d3849698b9f8e4a113
AbdielGE <86158145+AbdielGarc1a@users.noreply.github.com>
2022-03-16 04:50:13 -0600

    Add files via upload

---
+++ Bloc de Notas/Bloc de Notas/AppCore/IServices/IArchivoServices.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+   public interface IArchivoServices: IModel <DirectoryInfo>
+    {
+        bool Ruta(string path);
+
+        DirectoryInfo get(string path);
+    }
+}
---
+++ Bloc de Notas/Bloc de Notas/AppCore/Services/BaseServices.cs
@@ -0,0 +1,32 @@
+﻿using AppCore.IServices;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+   public abstract class BaseServices <T> : IServices<T>
+    {
+        private IModel<T> model;
+        private IArchivoServices archSer;
+
+        protected BaseServices(IModel<T> Archmodel)
+        {
+            this.model = Archmodel;
+        }
+
+        protected BaseServices(IArchivoServices archSer)
+        {
+            this.archSer = archSer;
+        }
+
+        public T Crear(string t)
+        {
+            return model.Crear(t);
+        }
+
+    }
+}
